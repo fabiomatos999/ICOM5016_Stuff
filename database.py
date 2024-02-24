e70258022ac3da58cd665153dfff88fd943e90d2
fabiomatos999 <fabio.matos@upr.edu>
2024-02-24 14:27:51 -0400

    Document database module.

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
-import psycopg2
+"""Database initialization and connection module.
 
-DB_NAME = "db"
-DB_USER = "uwu"
-DB_PASS = "uwu"
-DB_HOST = "127.0.0.1"
-DB_PORT = "5432"
+This module was developed for the term project - Hotel Analytics Systems for
+CIIC4060/ICOM 5016.
+"""
+import psycopg2
 
 
 class DatabaseConnection:
+    """Create database tables and connect to database."""
 
-    def __init__(self):
+    def __init__(self, DB_NAME: str, DB_USER: str, DB_PASS: str, DB_HOST: str,
+                 DB_PORT: str):
+        """Create DatabaseConnection object and tables if they do not exist."""
         with psycopg2.connect(database=DB_NAME,
                               user=DB_USER,
                               password=DB_PASS,
@@ -17,16 +19,18 @@ class DatabaseConnection:
                               port=DB_PORT) as conn:
             self.conn = conn
             self.cursor = conn.cursor()
-            self.create_rooms_table()
-            self.create_reserve_table()
+            self.createRoomTable()
+            self.createReserveTable()
 
-    def create_rooms_table(self):
+    def createRoomTable(self):
+        """Create Room table if it does not already exist."""
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS Room (rid SERIAL,
                 hid INTEGER NOT NULL, rdid INTEGER NOT NULL,
                 rprice REAL NOT NULL);""")
         self.conn.commit()
 
-    def create_reserve_table(self):
+    def createReserveTable(self):
+        """Create Reserve table if it does not already exist."""
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS Reserve (reid SERIAL,
             ruid INTEGER NOT NULL,
             clid INTEGER NOT NULL,
@@ -36,4 +40,5 @@ class DatabaseConnection:
         self.conn.commit()
 
     def __del__(self):
+        """Close database connection when object is destroyed."""
         self.conn.close()
